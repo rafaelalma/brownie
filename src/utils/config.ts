233d0cd84481b32647f0e498e35ab368a38be844
rafaelalma <rafaelalma92@gmail.com>
2022-09-27 00:15:32 +0200

    Install jest

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
-export default { PORT, MONGODB_URI };
+export default { PORT, MONGODB_URI }
